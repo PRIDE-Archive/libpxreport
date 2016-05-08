6c56d46287f292ba850754b6b3a4ca0e5f030fbb
Manuel Bernal Llinares <mbdebian@gmail.com>
2016-05-08 18:54:33 +0100

    main dummy App

---
+++ src/main/java/uk/ac/ebi/pride/px/App.java
@@ -0,0 +1,18 @@
+package uk.ac.ebi.pride.px;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
+
+    public static void main( String[] args )
+    {
+        logger.info("Hello!");
+    }
+}
